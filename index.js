4d2de556648418239f82ecf662deeb5c9da45452
haruka231 <sixthsense4545@gmail.com>
2023-02-08 16:08:49 +0800

    added files

---
+++ index.js
@@ -0,0 +1,31 @@
+//var x=5;
+//var y=10;
+//var sum=x*y;
+//document.write(sum);
+
+//var b=80.5;
+//var c=4.25e+6;
+//var d= 4.25e-6;
+//document.write(a+"<br>"+b+"<br>"+c+"<br>"+d)
+
+//var a=25;
+
+//var b=80.5;
+//var c=4.25e+6;
+//var d= 4.25e-6;
+//document.write(a+"<br>"+b+"<br>"+c+"<br>"+d)
+
+//var a="let's have a cup of coffee.",b='he said "hello" and left.',c='we\'ll never give up.';
+
+//document.write(a+"<br>"+b+"<br>"+c)
+
+//var name="francis", age=21,  status="single";
+//document.write(name+" " + age+"\n")
+//document.write(status+"<br>")
+
+var sum=function(){
+    var total =   parseInt(document.getElementById('magic').value)+ parseInt( document.getElementById('text2').value);
+return alert("the total is "+total);
+}
+var student="fname:francis,lname:borinaga,middle:Omega,age:21;
+document.write(student);
---
+++ js/index.js
@@ -0,0 +1,6 @@
+<script>
+var x=5;
+var y=10;
+var sum=x*y;
+document.write(sum);
+</script>
